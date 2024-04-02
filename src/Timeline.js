897ddf81d235f7b36d284d1f2b49173b9034d016
Fein <636592@bah.com>
2024-04-02 16:19:56 -0400

    Added whole team

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,8 +1,6 @@
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import logo from "./logo.svg";
-import KatrinaZell from './Headshots/KatrinaZell.png'
-import MichelleCody from './Headshots/MichelleCody.png'
 import DipenPatel from './Headshots/DipenPatel.png'
 import JoshDunlap from './Headshots/JoshDunlap.png'
 import KenKratz from './Headshots/KenKratz.png'
@@ -24,6 +22,25 @@ import EmilyPham from './Headshots/EmilyPham.png'
 import LilaZapata from './Headshots/LilaZapata.png'
 import BinuMatthew from './Headshots/BinuMatthew.png'
 import JoelKonitzer from './Headshots/JoelKonitzer.png'
+import ElenaKuenzel from './Headshots/ElenaKuenzel.png'
+import AnaCarlaCosta from './Headshots/AnaCarlaCosta.png'
+import SamirYuja from './Headshots/SamirYuja.png'
+import VincentGlorioso from './Headshots/VincentGlorioso.png'
+import BrandonKonkel from './Headshots/BrandonKonkel.png'
+import MattEpstein from './Headshots/MattEpstein.png'
+import AlecCrowell from './Headshots/AlecCrowell.png'
+import ChrisFalter from './Headshots/ChrisFalter.png'
+import JeffGross from './Headshots/JeffGross.png'
+import DanielPuskas from './Headshots/DanielPuskas.png'
+import MorganJacobi from './Headshots/MorganJacobi.png'
+import KarlSchliep from './Headshots/KarlSchliep.png'
+import ArjunPaudel from './Headshots/ArjunPaudel.png'
+import LindsayLienau from './Headshots/LindsayLienau.png'
+import TaylorHogge from './Headshots/TaylorHogge.png'
+import SebastianRanasinghe from './Headshots/SebastianRanasinghe.png'
+import RebeccaKurz from './Headshots/RebeccaKurz.png'
+import KatrinaZell from './Headshots/KatrinaZell.png'
+import MichelleCody from './Headshots/MichelleCody.png'
 import Dandelion from './Flowers/dandelion.png'
 import Daisy from './Flowers/daisy.png'
 import Poppy from './Flowers/poppy.png'
@@ -32,13 +49,216 @@ import Dahlia from './Flowers/dahlia.png'
 import Rose from './Flowers/rose.png'
 import Flower from './Flowers/flower.png'
 import Sunflower from './Flowers/sunflower.png'
+import Orchid from './Flowers/orchid.png'
+import PlumBlossom from './Flowers/PlumBlossom.png'
 function Timeline() {
     return (
         <VerticalTimeline>
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                contentStyle={{background: '#F0E130', color: '#000000'}}
+                date={<div style={{marginTop: -30}}><h1>November 11, 2019</h1></div>}
+                iconStyle={{background: '#87CEEB', color: '#000000'}}
+                contentStyle={{background: '#87CEEB', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Flower}/><h4 style={{marginTop: 4}}><i></i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={ElenaKuenzel}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Elena Kuenzel</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Cheif Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>December 7, 2020</h1></div>}
+                iconStyle={{background: '#ff878d', color: '#000000'}}
+                contentStyle={{background: '#ff878d', color: '#000000'}}
+                icon={<div><img style={{height: 50, width: 50, margin: 4}} src={Tulip}/><h4 style={{marginTop: 4}}>
+                    <i>Tulip</i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={AnaCarlaCosta}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Ana Carla Costa</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>October 11, 2021</h1></div>}
+                iconStyle={{background: '#87CEEB', color: '#000000'}}
+                contentStyle={{background: '#87CEEB', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Flower}/><h4 style={{marginTop: 4}}><i></i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={SamirYuja}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Samir Yuja</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Lead Technologist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>October 19, 2021</h1></div>}
+                iconStyle={{background: '#87CEEB', color: '#000000'}}
+                contentStyle={{background: '#87CEEB', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Flower}/><h4 style={{marginTop: 4}}><i></i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={VincentGlorioso}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Vincent Glorioso</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>January 10, 2022</h1></div>}
+                iconStyle={{background: '#ebd582', color: '#000000'}}
+                contentStyle={{background: '#ebd582', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Daisy}/><h4 style={{marginTop: 4}}><i>Daisy</i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={BrandonKonkel}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Brandon Konkel</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Lead Engineer</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>April 1, 2022</h1></div>}
+                iconStyle={{background: '#87CEEB', color: '#000000'}}
+                contentStyle={{background: '#87CEEB', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Flower}/><h4 style={{marginTop: 4}}><i></i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={MattEpstein}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Matt Epstein</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>April 11, 2022</h1></div>}
+                iconStyle={{background: '#673147', color: '#000000'}}
+                contentStyle={{background: '#673147', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={PlumBlossom}/><h4 style={{marginTop: 4}}><i>Plum Blossom</i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={AlecCrowell}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Alec Crowell</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>May 17, 2022</h1></div>}
+                iconStyle={{background: '#ebd582', color: '#000000'}}
+                contentStyle={{background: '#ebd582', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Daisy}/><h4 style={{marginTop: 4}}><i>Daisy</i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={MichelleCody}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Michelle Cody</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
 
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>August 1, 2022</h1></div>}
+                iconStyle={{background: '#87CEEB', color: '#000000'}}
+                contentStyle={{background: '#87CEEB', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Flower}/><h4 style={{marginTop: 4}}><i></i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={ChrisFalter}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Chris Falter</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                iconStyle={{background: '#87CEEB', color: '#000000'}}
+                contentStyle={{background: '#87CEEB', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Flower}/><h4 style={{marginTop: 4}}><i></i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={JeffGross}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Jeff Gross</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                iconStyle={{background: '#ebd582', color: '#000000'}}
+                contentStyle={{background: '#ebd582', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Daisy}/><h4 style={{marginTop: 4}}><i>Daisy</i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={DanielPuskas}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Daniel Puskas</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>August 15, 2022</h1></div>}
+                iconStyle={{background: '#FF007F', color: '#000000'}}
+                contentStyle={{background: '#FF007F', color: '#000000'}}
+                icon={<div><img style={{height: 50, width: 50, margin: 4}} src={Rose}/><h4 style={{marginTop: 4}}>
+                    <i>Rose</i></h4></div>}
+            >
+
+                <img style={{height: 112, width: 105}} src={MorganJacobi}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Morgan Jacobi</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>October 17, 2022</h1></div>}
+                iconStyle={{background: '#E35335', color: '#000000'}}
+                contentStyle={{background: '#E35335', color: '#000000'}}
+                icon={<div><img style={{height: 50, width: 50, margin: 4}} src={Poppy}/><h4 style={{marginTop: 4}}>
+                    <i>Poppy</i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={KarlSchliep}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Karl Schliep</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>November 14, 2022</h1></div>}
+                iconStyle={{background: '#87CEEB', color: '#000000'}}
+                contentStyle={{background: '#87CEEB', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Flower}/><h4 style={{marginTop: 4}}><i></i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={ArjunPaudel}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Arjun Paudel</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>November 28, 2022</h1></div>}
+                iconStyle={{background: '#ebd582', color: '#000000'}}
+                contentStyle={{background: '#ebd582', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Daisy}/><h4 style={{marginTop: 4}}><i>Daisy</i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={LindsayLienau}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Lindsay Lienau</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>January 9, 2023</h1></div>}
+                iconStyle={{background: '#ff878d', color: '#000000'}}
+                contentStyle={{background: '#ff878d', color: '#000000'}}
+                icon={<div><img style={{height: 50, width: 50, margin: 4}} src={Tulip}/><h4 style={{marginTop: 4}}>
+                    <i>Tulip</i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={TaylorHogge}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Taylor Hogge</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>January 17, 2023</h1></div>}
+                iconStyle={{background: '#87CEEB', color: '#000000'}}
+                contentStyle={{background: '#87CEEB', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Flower}/><h4 style={{marginTop: 4}}><i></i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={SebastianRanasinghe}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Sebastian Ranasinghe</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={<div style={{marginTop: -30}}><h1>February 1, 2023</h1></div>}
+                iconStyle={{background: '#ebd582', color: '#000000'}}
+                contentStyle={{background: '#ebd582', color: '#000000'}}
+                icon={<div><img style={{margin: 4}} src={Daisy}/><h4 style={{marginTop: 4}}><i>Daisy</i></h4></div>}
+            >
+                <img style={{height: 112, width: 105}} src={RebeccaKurz}/>
+                <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Rebecca Kurz</h3>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
+            </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                contentStyle={{background: '#F0E130', color: '#000000'}}
                 date={<div style={{marginTop: -30}}><h1>April 10, 2023</h1></div>}
                 iconStyle={{background: '#F0E130', color: '#000000'}}
                 icon={<div><img style={{height: 50, width: 50, margin: 4}} src={Dandelion}/><h4 style={{marginLeft: -5, marginTop: 4}}>
@@ -54,15 +274,10 @@ function Timeline() {
                         iconStyle={{background: '#87CEEB', color: '#000000'}}
                 contentStyle={{background: '#87CEEB', color: '#000000'}}
                 icon={<div><img style={{margin: 4}} src={Flower}/><h4 style={{marginTop: 4}}><i></i></h4></div>}
-
-                // icon={<WorkIcon/>}
             >
                 <img src={DipenPatel}/>
-
                 <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Dipen Patel</h3>
-                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead
-                    Scientist</h4>
-
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Scientist</h4>
             </VerticalTimelineElement>
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
@@ -86,7 +301,7 @@ function Timeline() {
             >
                 <img src={MadelineStilley}/>
                 <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Madeline Stilley</h3>
-                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Lead Associate</h4>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Senior Lead Technologist</h4>
 
             </VerticalTimelineElement>
             <VerticalTimelineElement
@@ -173,7 +388,7 @@ function Timeline() {
             >
                 <img src={FarhadSaffaraval}/>
                 <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Farhad Saffaraval</h3>
-                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Data Science SME</h4>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Lead Data Scientist</h4>
 
             </VerticalTimelineElement>
             <VerticalTimelineElement
@@ -237,7 +452,7 @@ function Timeline() {
             >
                 <img src={LilaZapata}/>
                 <h3 style={{color: "#000000"}} className="vertical-timeline-element-title">Lila Zapata</h3>
-                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">ML Engineer</h4>
+                <h4 style={{color: "#000000"}} className="vertical-timeline-element-subtitle">Lead Data Engineer</h4>
             </VerticalTimelineElement>
             <VerticalTimelineElement
                 date={<div style={{marginTop: -30}}><h1>January 16th, 2024</h1></div>}
